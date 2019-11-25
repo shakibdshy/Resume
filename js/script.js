c8a254eb03f63fb509fceac4674851cb158f6dfc
Shakib <shakibdshy@gmail.com>
2019-11-25 19:58:12 +0600

    Page Transition Add

--- js/script.js
+++ js/script.js
@@ -8,46 +8,55 @@ Version      : 1.0
 {
 	
 	"use strict";
-	// Preloader
-	// jQuery(window).on('load', function() {
-	// 	preloader();
-		
-	// 	// mCustomScrollbar
-	// 	jQuery(".header-inner").mCustomScrollbar();
-	// 	jQuery(".pt-page").mCustomScrollbar();		
-		
-	// });
+	
 
 	// Author Contact Tooltip 
 	$('[data-toggle="tooltip"]').tooltip()
 	
-	// mCustomScrollbar
-	$(window).on("load", function () {
-		$(".sidebar").mCustomScrollbar();
+	// Preloader
+	jQuery(window).on('load', function () {
+		// preloader();
+
+		// mCustomScrollbar
+		jQuery(".header").mCustomScrollbar();
+		jQuery(".pt-page").mCustomScrollbar();
+
 	});
-	
 
 	// Sidebar Opener
 	$('#menu .mobile-menu-opener').on('click', function (e) {
 		e.preventDefault();
 		var div = $('#menu');
-		if (div.css('left') === '-142px') {
+		if (div.css('left') === '-220px') {
 			$('#menu').animate({
 				left: '0px'
 			});
 		} else {
 			$('#menu').animate({
-				left: '-142px'
+				left: '-220px'
 			});
 		}
 	});
 
 	// Mobile Menu 
-	$('nav li a').on('click', function (e) {
+	$('#navbar li a').on('click', function (e) {
 		e.preventDefault();
 		if ($(window).width() < 767) {
 			$('#menu').animate({
-				left: '-142px'
+				left: '-220px'
+			});
+		} else {
+			$('#menu').animate({
+				left: '0'
+			});
+		}
+	});
+
+	$('.content').on('click', function (e) {
+		//e.preventDefault();
+		if ($(window).width() < 767) {
+			$('#menu').animate({
+				left: '-220px'
 			});
 		} else {
 			$('#menu').animate({
@@ -60,6 +69,7 @@ Version      : 1.0
 	new WOW().init();
 
 	// Language
+	
 	$("#english").percircle({
 		progressBarColor: "#F9CA2B",
 	});
