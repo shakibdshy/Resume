3a17fdff7120c185bc9d6ca2fe785c598e62fed6
Shakib <shakibdshy@gmail.com>
2019-12-02 22:27:19 +0600

    Homepage 2 Menu Update

--- js/script.js
+++ js/script.js
@@ -188,6 +188,37 @@ Version      : 1.0
 		});
 	});
 
+	
+
+	function getPoint(point, i, a, smoothing) {
+		let cp = (current, previous, next, reverse) => {
+			let p = previous || current,
+				n = next || current,
+				o = {
+					length: Math.sqrt(Math.pow(n[0] - p[0], 2) + Math.pow(n[1] - p[1], 2)),
+					angle: Math.atan2(n[1] - p[1], n[0] - p[0])
+				},
+				angle = o.angle + (reverse ? Math.PI : 0),
+				length = o.length * smoothing;
+			return [current[0] + Math.cos(angle) * length, current[1] + Math.sin(angle) * length];
+		},
+			cps = cp(a[i - 1], a[i - 2], point, false),
+			cpe = cp(point, a[i - 1], a[i + 1], true);
+		return `C ${cps[0]},${cps[1]} ${cpe[0]},${cpe[1]} ${point[0]},${point[1]}`;
+	}
+
+	function getPath(update, smoothing, pointsNew) {
+		let points = pointsNew ? pointsNew : [
+			[4, 12],
+			[12, update],
+			[20, 12]
+		],
+			d = points.reduce((acc, point, i, a) => i === 0 ? `M ${point[0]},${point[1]}` : `${acc} ${getPoint(point, i, a, smoothing)}`, '');
+		return `<path d="${d}" />`;
+	}
+
+
+	// GMaps
 	function basicmap() {
 		// Basic options for a simple Google Map
 		// For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
